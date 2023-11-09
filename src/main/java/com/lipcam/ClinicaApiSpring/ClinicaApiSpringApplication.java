5406b50667af13b5657a8237d44ae523e4cdbc52
LipCam <lipcam@hotmail.com.br>
2023-11-09 08:26:11 -0300

    Commit inicial

---
+++ src/main/java/com/lipcam/ClinicaApiSpring/ClinicaApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lipcam.ClinicaApiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lipcam/ClinicaApiSpring/ClinicaApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipcam.ClinicaApiSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
